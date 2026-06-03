8fd905f551894d58f8de2dd12f5434ea03e2209a
shivam1108-06 <shivamkotak118@gmail.com>
2026-06-03 23:38:25 +0530

    Add complete and delete task feature

--- app.py
+++ app.py
@@ -8,10 +8,28 @@ tasks = []
 def home():
     if request.method == "POST":
         task = request.form["task"]
-        tasks.append(task)
+
+        tasks.append({
+            "name": task,
+            "completed": False
+        })
+
         return redirect("/")
 
     return render_template("index.html", tasks=tasks)
+
+
+@app.route("/delete/<int:index>")
+def delete(index):
+    tasks.pop(index)
+    return redirect("/")
+
+
+@app.route("/complete/<int:index>")
+def complete(index):
+    tasks[index]["completed"] = True
+    return redirect("/")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
