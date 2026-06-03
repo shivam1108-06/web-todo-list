9c347aa4ac0b13fee8932d0319b13fe35f85c9e2
shivam1108-06 <shivamkotak118@gmail.com>
2026-06-03 23:43:56 +0530

    Add SQLite database support

--- app.py
+++ app.py
@@ -1,33 +1,59 @@
 from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-tasks = []
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200))
+    completed = db.Column(db.Boolean, default=False)
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    if request.method == "POST":
-        task = request.form["task"]
 
-        tasks.append({
-            "name": task,
-            "completed": False
-        })
+    if request.method == "POST":
+        task_name = request.form["task"]
+
+        new_task = Task(name=task_name)
+
+        db.session.add(new_task)
+        db.session.commit()
 
         return redirect("/")
+
+    tasks = Task.query.all()
 
     return render_template("index.html", tasks=tasks)
 
 
-@app.route("/delete/<int:index>")
-def delete(index):
-    tasks.pop(index)
+@app.route("/delete/<int:id>")
+def delete(id):
+
+    task = Task.query.get(id)
+
+    db.session.delete(task)
+    db.session.commit()
+
     return redirect("/")
 
 
-@app.route("/complete/<int:index>")
-def complete(index):
-    tasks[index]["completed"] = True
+@app.route("/complete/<int:id>")
+def complete(id):
+
+    task = Task.query.get(id)
+
+    task.completed = True
+
+    db.session.commit()
+
     return redirect("/")
 
 
