de85b761d9a02b28efffa9d667dc53a4e80f9e34
shivam1108-06 <shivamkotak118@gmail.com>
2026-06-03 15:46:01 +0530

    Day 2: Add task creation feature

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route("/")
+tasks = []
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return "Hello, To-Do App!"
+    if request.method == "POST":
+        task = request.form["task"]
+        tasks.append(task)
+        return redirect("/")
+
+    return render_template("index.html", tasks=tasks)
 
 if __name__ == "__main__":
     app.run(debug=True)
